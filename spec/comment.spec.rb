3151fbb8b831cb99b6afecf65991301bae8892c8
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-08-26 13:51:16 +0300

    Fix linters errors

--- spec/comment.spec.rb
+++ spec/comment.spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   subject(:user) { User.new(Name: 'John Doe', Photo: 'hhddudui', Bio: 'I am John Doe', PostsCounter: 0) }
-  let(:post) {Post.new(Author_id: user.id, Title: 'My first post', Text: 'This is my first post', CommentsCounter: 0, LikesCounter: 0) }
-  let(:comment) {Comment.new(User_id: user.id, Post_id: post.id, Text: 'This is my first comment')}
+  let(:post) do
+    Post.new(Author_id: user.id, Title: 'My first post', Text: 'This is my first post', CommentsCounter: 0,
+             LikesCounter: 0)
+  end
+  let(:comment) { Comment.new(User_id: user.id, Post_id: post.id, Text: 'This is my first comment') }
 
-  before {user.save!}
-  before {post.save!}
-  before {comment.save!}
+  before { user.save! }
+  before { post.save! }
+  before { comment.save! }
 
-  it "update_post_comment_count should be called automatically on saveing comments" do
+  it 'update_post_comment_count should be called automatically on saveing comments' do
     post.reload
     expect(post.CommentsCounter).to eq(1)
   end
--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   subject(:user) { User.new(Name: 'John Doe', Photo: 'hhddudui', Bio: 'I am John Doe', PostsCounter: 0) }
-  let(:post) {Post.new(Author_id: user.id, Title: 'My first post', Text: 'This is my first post', CommentsCounter: 0, LikesCounter: 0) }
-  let(:like) {Like.new(User_id: user.id, Post_id: post.id)}
+  let(:post) do
+    Post.new(Author_id: user.id, Title: 'My first post', Text: 'This is my first post', CommentsCounter: 0,
+             LikesCounter: 0)
+  end
+  let(:like) { Like.new(User_id: user.id, Post_id: post.id) }
 
-  before {user.save!}
-  before {post.save!}
-  before {like.save!}
+  before { user.save! }
+  before { post.save! }
+  before { like.save! }
 
-  it "update_post_like_count should be called automatically on saveing likes" do
+  it 'update_post_like_count should be called automatically on saveing likes' do
     post.reload
     expect(post.LikesCounter).to eq(1)
   end
