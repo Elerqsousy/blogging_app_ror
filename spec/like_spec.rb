f5e645e057701f87a38f3346b9da2bd94bb52699
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-08-26 13:50:38 +0300

    Add model testing

---
+++ spec/like_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  subject(:user) { User.new(Name: 'John Doe', Photo: 'hhddudui', Bio: 'I am John Doe', PostsCounter: 0) }
+  let(:post) {Post.new(Author_id: user.id, Title: 'My first post', Text: 'This is my first post', CommentsCounter: 0, LikesCounter: 0) }
+  let(:like) {Like.new(User_id: user.id, Post_id: post.id)}
+
+  before {user.save!}
+  before {post.save!}
+  before {like.save!}
+
+  it "update_post_like_count should be called automatically on saveing likes" do
+    post.reload
+    expect(post.LikesCounter).to eq(1)
+  end
+end
